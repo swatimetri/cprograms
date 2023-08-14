711f97bc8d283c61c5a89fb9ac7a3a96ec515e04
swati <118157700+swatimetri@users.noreply.github.com>
2023-08-14 14:33:46 +0530

    Create age calculator.c

---
+++ age calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int born;
+    printf("enter your born year:\n");
+    scanf("%d",&born);
+    int age;
+    age=2022-born;
+    printf("your age is:%d",age);
+    
+}
